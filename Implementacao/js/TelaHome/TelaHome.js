3849f361f3ef90dcbfec15c82d318fab7cf68f71
EduardoGarzon <luizeduardo3131@gmail.com>
2023-05-21 17:53:36 -0300

    Tela Home.
    Implementacao frontend da tela home.

--- Implementacao/js/TelaHome/TelaHome.js
+++ Implementacao/js/TelaHome/TelaHome.js
@@ -1,13 +1,28 @@
+/* Código para barra lateral de navegacao */
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(bx-menu);
+        this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link) => {
+            link.style.animation ? (link.style.animation = "") : (link.style.animation = 'navLinkFade 0.5s ease forwards 0.3s');
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
 
     addClickEvent() {
-        this.mobileMenu.addClickListener("click", () => console.log("hey."));
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
     init() {
@@ -19,8 +34,8 @@ class MobileNavbar {
 }
 
 const mobileNavbar = new MobileNavbar(
-    ".bx-menu", 
-    ".nav-list", 
-    ".nav-list li"
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
 );
 mobileNavbar.init();
